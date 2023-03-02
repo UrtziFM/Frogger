e874e962dc6f54d8107c9550de6f750efd0b36a7
u100247 <u100247@MM0012B7.local>
2023-03-02 17:59:47 +0100

    time 42:45

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-button");
 const squares = document.querySelectorAll(".grid div");
+const logsLeft = document.querySelectorAll(".log-left");
 
 console.log(squares);
 let currentIndex = 76;
@@ -15,22 +16,35 @@ function moveFrog(e) {
 
     switch(e.key){
         case "ArrowLeft":
-            console.log("move left");
-            currentIndex -= 1;
+            if(currentIndex % width !== 0) {
+                currentIndex -= 1};
             break
         case "ArrowRight":
-            console.log("move right");
-            currentIndex += 1;
+            if(currentIndex % width < width -1) { 
+                currentIndex += 1};
             break
         case "ArrowUp":
-            console.log("move up");
-            currentIndex -= width;
+            if(currentIndex - width >= 0) {
+                currentIndex -= width};
             break
         case "ArrowDown":
-            console.log("move down");
-            currentIndex += width;
+            if(currentIndex + width < width * width) {
+                currentIndex += width};
             break
     }
     squares[currentIndex].classList.add("frog");
 }
 document.addEventListener("keyup", moveFrog);
+
+function autoMoveLogs(){
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+}
+
+function moveLogLeft(logLeft){
+    switch(true) {
+        case logLeft.classList.contains("l1"):
+            logLeft.classList.remove("l1");
+            logLeft.classList.add("l2");
+            break
+    }
+}
